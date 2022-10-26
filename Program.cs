55130782219d5ba7ff3536736bb7c8515cb2590a
KatiaRassadina <rassadinaekaterina877@gmail.com>
2022-10-26 22:28:55 +0300

    task done

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа задает массив из 8 элементов и наполняет его произвольными элементами
+int[] array = new int[8];
+void FillArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(2,9);
+}
+}
+void PrintArray(int[] array)
+{
+for (int i = 0; i < array.Length - 1; i++)
+{
+    Console.Write($"{array[i]}, ");
+}
+Console.Write($"{array[array.Length - 1]} ");
+}
+PrintArray(array);
+Console.WriteLine();
+FillArray(array);
+PrintArray(array);
